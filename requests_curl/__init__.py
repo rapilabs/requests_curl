36aef05ab964d9347e4fd57a3dc7decc57d8f82a
David Sanders <dsanders@rapilabs.com>
2016-09-13 01:03:26 +1000

    Initial code commit

---
+++ requests_curl/__init__.py
@@ -0,0 +1,86 @@
+import logging
+import sys
+import urllib
+from json import dumps
+
+import requests
+
+logger = logging.getLogger('requests_curl')
+
+functions = (
+    'request',
+    'get',
+    'options',
+    'head',
+    'post',
+    'put',
+    'patch',
+    'delete',
+)
+originals = {}
+
+
+def request(method, url, **kwargs):
+    response = originals['request'](method, url, **kwargs)
+    logger.debug('curl -X {} {}'.format(response.request.method, response.url))
+    return response
+
+
+def get(url, params=None, **kwargs):
+    response = originals['get'](url, params=params, **kwargs)
+    logger.debug('curl -L {}'.format(response.url))
+    return response
+
+
+def options(url, **kwargs):
+    response = originals['options'](url, **kwargs)
+    logger.debug('curl -L -X OPTIONS {}'.format(response.url))
+    return response
+
+
+def head(url, **kwargs):
+    response = originals['head'](url, **kwargs)
+    logger.debug('curl -L -X HEAD {}'.format(response.url))
+    return response
+
+
+def post(url, data=None, json=None, **kwargs):
+    response = originals['post'](url, data=data, json=json, **kwargs)
+
+    data_option = ''
+    if data:
+        data_option = "--data-urlencode '{}'".format(urllib.parse.urlencode(data))
+    elif json:
+        data_option = "--data '{}'".format(dumps(json))
+
+    logger.debug('curl -X POST {} {}'.format(data_option, response.url))
+    return response
+
+
+def put(url, data=None, **kwargs):
+    response = originals['put'](url, data=data, **kwargs)
+    data_option = ''
+    if data:
+        data_option = "--data-urlencode '{}'".format(urllib.parse.urlencode(data))
+    logger.debug('curl -X PUT {} {}'.format(data_option, response.url))
+    return response
+
+
+def patch(url, data=None, **kwargs):
+    response = originals['patch'](url, data=data, **kwargs)
+    data_option = ''
+    if data:
+        data_option = "--data-urlencode '{}'".format(urllib.parse.urlencode(data))
+    logger.debug('curl -X PATCH {} {}'.format(data_option, response.url))
+    return response
+
+
+def delete(url, **kwargs):
+    response = originals['delete'](url, **kwargs)
+    logger.debug('curl -X DELETE {}'.format(response.url))
+    return response
+
+
+for func in functions:
+    originals[func] = getattr(requests, func)
+    setattr(requests, func, getattr(sys.modules[__name__], func))
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='requests_curl',
+    version='0.1.0',
+    description='Log curl requests for each call to requests',
+    author='David Sanders',
+    author_email='dsanders@rapilabs.com',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+    ],
+)
